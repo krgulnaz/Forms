2d0b7010b6f0d1e20d2771d90bd91af4453c3619
krgulnaz <krgulnoza@gmail.com>
2021-11-14 21:47:17 +0100

    Added forms

---
+++ WorkWıthTheForms/О программе.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WorkWıthTheForms
+{
+    partial class О_программе
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(43, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(283, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Программа создана в Borland C# Builder for the Microsoft .Net Framework";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(280, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // О_программе
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 130);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "О_программе";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "О_программе";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
